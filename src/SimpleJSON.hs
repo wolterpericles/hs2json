cfec3f601cd373a8bfada516d0fd1a6471523fba
Wolter <wpericles1@gmail.com>
2020-07-09 02:15:33 -0300

    SimpleJSON

--- src/SimpleJSON.hs
+++ src/SimpleJSON.hs
@@ -1,2 +1,10 @@
 -- src/SimpleJSON.hs
 module SimpleJSON where
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+              deriving (Eq, Ord, Show)
