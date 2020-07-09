847b1e2a3828cee295b5ca86a56d831bee0acfac
Wolter <wpericles1@gmail.com>
2020-07-09 16:29:42 -0300

    Módulo Prettify

---
+++ src/Prettify.hs
@@ -0,0 +1,17 @@
+-- file: src/Prettify.hs
+module Prettify where
+
+import SimpleJSON
+
+data Doc = ToBeDefined
+         deriving (Show)
+
+string :: String -> Doc
+string str = undefined
+
+text :: String -> Doc
+text str = undefined
+
+double :: Double -> Doc
+double num = undefined
+
