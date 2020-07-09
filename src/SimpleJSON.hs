1e542bdd56dfccef50686e7018a4e657bfde93c4
Wolter <wpericles1@gmail.com>
2020-07-09 02:26:26 -0300

    Funções acessoras

--- src/SimpleJSON.hs
+++ src/SimpleJSON.hs
@@ -12,3 +12,20 @@ data JValue = JString String
 getString :: JValue -> Maybe String
 getString (JString s) = Just s
 getString _           = Nothing
+
+getInt (JNumber n) = Just (truncate n)
+getInt _           = Nothing
+
+getDouble (JNumber n) = Just n
+getDouble _           = Nothing
+
+getBool (JBool b) = Just b
+getBool _         = Nothing
+
+getObject (JObject o) = Just o
+getObject _           = Nothing
+
+getArray (JArray a) = Just a
+getArray _          = Nothing
+
+isNull v            = v == JNull
