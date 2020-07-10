ba574136b09052e4af8bfabf4d2408ea13a17fbb
Wolter <wpericles1@gmail.com>
2020-07-10 10:30:29 -0300

    Atualizando Prettify

--- src/Prettify.hs
+++ src/Prettify.hs
@@ -15,12 +15,7 @@ text :: String -> Doc
 text str = undefined
 
 double :: Double -> Doc
-double num = undefined
-
-{-
-string :: String -> Doc
-string = enclose '"' '"' . hcat . map oneChar
--}
+double num = undefinedr3
 
 (<>) :: Doc -> Doc -> Doc
 a <> b = undefined
@@ -30,14 +25,6 @@ char c = undefined
 
 hcat :: [Doc] -> Doc
 hcat xs = undefined
-{-
-oneChar :: Char -> Doc
-oneChar c = case lookup c simpleEscapes of
-              Just r -> text r
-              Nothing | mustEscape c -> hexEscape c
-                      | otherwise    -> char c
-    where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
--}
 
 simpleEscapes :: [(Char, String)]
 simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
