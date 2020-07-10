0027ef39d341895d13a504b8145a502a6d95192a
Wolter <wpericles1@gmail.com>
2020-07-10 07:06:15 -0300

    Imprimindo String

--- src/Prettify.hs
+++ src/Prettify.hs
@@ -1,13 +1,15 @@
 -- file: src/Prettify.hs
 module Prettify where
+import Prelude hiding ((<>))
 
 import SimpleJSON
+import PrettyJSON
 
 data Doc = ToBeDefined
          deriving (Show)
 
-string :: String -> Doc
-string str = undefined
+--string :: String -> Doc
+--string str = undefined
 
 text :: String -> Doc
 text str = undefined
@@ -17,3 +19,23 @@ double num = undefined
 
 string :: String -> Doc
 string = enclose '"' '"' . hcat . map oneChar
+
+(<>) :: Doc -> Doc -> Doc
+a <> b = undefined
+
+char :: Char -> Doc
+char c = undefined
+
+hcat :: [Doc] -> Doc
+hcat xs = undefined
+
+oneChar :: Char -> Doc
+oneChar c = case lookup c simpleEscapes of
+              Just r -> text r
+              Nothing | mustEscape c -> hexEscape c
+                      | otherwise    -> char c
+    where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
+
+simpleEscapes :: [(Char, String)]
+simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+    where ch a b = (a, ['\\',b])
--- src/PrettyJSON.hs
+++ src/PrettyJSON.hs
@@ -3,6 +3,7 @@ module PrettyJSON where
 
 import SimpleJSON
 import Prettify
+import Numeric (showHex)
 
 renderJValue :: JValue -> Doc
 renderJValue (JBool True)  = text "true"
@@ -13,3 +14,23 @@ renderJValue (JString str) = string str
 
 pointyString :: String -> Doc
 pointyString s = enclose '"' '"' (hcat (map oneChar s))
+
+enclose :: Char -> Char -> Doc -> Doc
+enclose left right x = char left <> x <> char right
+
+oneChar :: Char -> Doc
+oneChar c = case lookup c simpleEscapes of
+              Just r -> text r
+              Nothing | mustEscape c -> hexEscape c
+                      | otherwise    -> char c
+    where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
+
+simpleEscapes :: [(Char, String)]
+simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+    where ch a b = (a, ['\\',b])
+
+smallHex :: Int -> Doc
+smallHex x  = text "\\u"
+           <> text (replicate (4 - length h) '0')
+           <> text h
+    where h = showHex x ""
