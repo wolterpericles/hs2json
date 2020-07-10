7737464079ac675f7c906c2c137b5962987bad72
Wolter <wpericles1@gmail.com>
2020-07-10 11:30:00 -0300

    Finalizando hs2json

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 -- file: app/Main.hs
+
+{-
 module Main (main) where
 
 import SimpleJSON
+import Prettify
+import PrettyJSON
 
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+
+value = renderJValue $ JObject [("f", JNumber 1), ("q", JBool True)]
+main :: IO ()
+main = do
+    putStrLn (pretty 10 value)
+    putStrLn (pretty 20 value)
+    putStrLn (pretty 30 value)
+-}
+
+import SimpleJSON
+import Prettify
+import PrettyJSON
+
+value = renderJValue $ JObject[("nome", JString "Sergio Costa"), ("matricula", JNumber 123), ("status", JBool True)]
+
+main::IO()
+main = do
+    putStrLn(pretty 10 value)
